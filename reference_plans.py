1a132cce1b12574c15c516ac4d30e11c1d4637e0
Pranit-satnurkar <pranit.satnurkar@gmail.com>
2026-03-10 15:01:57 +0530

    Refactor: Clean up project clutter and update structure for repo sync

---
+++ reference_plans.py
@@ -0,0 +1,31 @@
+REFERENCE_PLANS = {
+    '2BHK': {
+        'facing': 'NW',
+        'style': 'modern',
+        'rooms': [
+            {'name': 'Kitchen',     'x_pct': 0.0,  'y_pct': 0.0,  'w_pct': 0.38, 'h_pct': 0.22},
+            {'name': 'Bedroom 1',   'x_pct': 0.38, 'y_pct': 0.0,  'w_pct': 0.35, 'h_pct': 0.28},
+            {'name': 'Toilet 1',    'x_pct': 0.73, 'y_pct': 0.0,  'w_pct': 0.27, 'h_pct': 0.13},
+            {'name': 'Bedroom 2',   'x_pct': 0.38, 'y_pct': 0.28, 'w_pct': 0.35, 'h_pct': 0.27},
+            {'name': 'Toilet 2',    'x_pct': 0.73, 'y_pct': 0.39, 'w_pct': 0.27, 'h_pct': 0.13},
+            {'name': 'Living Room', 'x_pct': 0.0,  'y_pct': 0.22, 'w_pct': 0.38, 'h_pct': 0.38},
+            {'name': 'Pooja',       'x_pct': 0.0,  'y_pct': 0.60, 'w_pct': 0.2,  'h_pct': 0.15},
+        ]
+    },
+    '3BHK': {
+        'facing': 'SW',
+        'style': 'modern',
+        'rooms': [
+            {'name': 'Bedroom 1',      'x_pct': 0.0,  'y_pct': 0.0,  'w_pct': 0.38, 'h_pct': 0.38},
+            {'name': 'Pooja',          'x_pct': 0.38, 'y_pct': 0.0,  'w_pct': 0.24, 'h_pct': 0.15},
+            {'name': 'Bedroom 2',      'x_pct': 0.62, 'y_pct': 0.0,  'w_pct': 0.38, 'h_pct': 0.38},
+            {'name': 'Toilet 1',       'x_pct': 0.0,  'y_pct': 0.28, 'w_pct': 0.17, 'h_pct': 0.15},
+            {'name': 'Passage',        'x_pct': 0.38, 'y_pct': 0.15, 'w_pct': 0.24, 'h_pct': 0.28},
+            {'name': 'Toilet 2',       'x_pct': 0.83, 'y_pct': 0.28, 'w_pct': 0.17, 'h_pct': 0.15},
+            {'name': 'Living Room',    'x_pct': 0.0,  'y_pct': 0.43, 'w_pct': 0.38, 'h_pct': 0.30},
+            {'name': 'Kitchen',        'x_pct': 0.38, 'y_pct': 0.43, 'w_pct': 0.62, 'h_pct': 0.25},
+            {'name': 'Master Bedroom', 'x_pct': 0.38, 'y_pct': 0.68, 'w_pct': 0.38, 'h_pct': 0.32},
+            {'name': 'Toilet 3',       'x_pct': 0.76, 'y_pct': 0.86, 'w_pct': 0.24, 'h_pct': 0.14},
+        ]
+    }
+}
